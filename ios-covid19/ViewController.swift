01b9a96dd95e427e13a4de577e8418210c3a488f
JIINHEO <gjwldls123@gmail.com>
2021-11-10 15:43:53 +0900

    First commit

---
+++ ios-covid19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-covid19
+//
+//  Created by 허지인 on 2021/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
